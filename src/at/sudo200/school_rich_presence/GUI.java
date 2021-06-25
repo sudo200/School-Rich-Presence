13f9bef2810f5cd800e504f4f3a93cdd2be37695
sudo200 <65343982+sudo200@users.noreply.github.com>
2021-06-25 19:03:19 +0200

    :art: Refactored code Added Labor

--- src/at/sudo200/school_rich_presence/GUI.java
+++ src/at/sudo200/school_rich_presence/GUI.java
@@ -6,13 +6,13 @@ import net.arikia.dev.drpc.DiscordRichPresence;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Locale;
+import java.util.Objects;
 
 public class GUI {
     public GUI(DiscordRichPresence presence) {
 
         JFrame frame = new JFrame();
         JButton[] buttons = {
-                new JButton("Deaktivieren"),
                 new JButton("Englisch"),
                 new JButton("GGP"),
                 new JButton("HWE"),
@@ -25,46 +25,46 @@ public class GUI {
                 new JButton("FSST"),
                 new JButton("Mathe"),
                 new JButton("Religion"),
-                new JButton("SOPK"),
                 new JButton("DIC"),
-                new JButton("Sport")
+                new JButton("Sport"),
+                new JButton("Labor"),
         };
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 20, 30));
         panel.setLayout(new GridLayout());
 
-        buttons[0].setSize(32, 32);
-        buttons[0].addActionListener(e -> {
+        JButton button = new JButton("Deaktivieren");
+        button.setSize(32, 32);
+        button.addActionListener(e -> {
             presence.clear();
             presence.largeImageKey = "default";
             presence.largeImageText = "Schule";
             DiscordRPC.discordUpdatePresence(presence);
         });
-        panel.add(buttons[0]);
+        panel.add(button);
 
-        for(int i = 1; i < buttons.length; i++) {
-            int finalI = i;
-            buttons[i].setSize(32, 32);
-            buttons[i].addActionListener(e -> {
-                presence.details = buttons[finalI].getText();
-                presence.largeImageText = buttons[finalI].getText();
+        for (JButton jButton : buttons) {
+            jButton.setSize(32, 32);
+            jButton.addActionListener(e -> {
+                presence.details = jButton.getText();
+                presence.largeImageText = jButton.getText();
                 presence.smallImageKey = "default";
-                presence.largeImageKey = buttons[finalI].getText().toLowerCase(Locale.ROOT);
-                presence.startTimestamp = System.currentTimeMillis()/1000;
+                presence.largeImageKey = jButton.getText().toLowerCase(Locale.ROOT);
+                presence.startTimestamp = System.currentTimeMillis() / 1000;
                 DiscordRPC.discordUpdatePresence(presence);
             });
-            panel.add(buttons[i]);
+            panel.add(jButton);
         }
 
         panel.setForeground(new Color(194, 194, 194));
         panel.setBackground(new Color(57, 57, 57));
 
         frame.add(panel, BorderLayout.CENTER);
-        frame.setIconImage(new ImageIcon(getClass().getResource("resources/icon.png")).getImage());
+        frame.setIconImage(new ImageIcon(Objects.requireNonNull(getClass().getResource("resources/icon.png"))).getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1920, 120);
         frame.setResizable(false);
-        frame.setTitle("Schule Rich Presence");
+        frame.setTitle("School Rich Presence");
         frame.setVisible(true);
 
     }
